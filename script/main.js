87da0a6cc76566bf6fc99db38d59cd065dc663f0
FahimX <sakibfahim270@gmail.com>
2025-03-01 13:21:50 +0800

    addin date and times

--- script/main.js
+++ script/main.js
@@ -1,36 +1,59 @@
 document.getElementById("btn-shop").addEventListener("click", function (evt) {
+  let time = getCurrentTime();
+  alert(`Board updated successfully at ${time}`);
   reduceTask("task-no");
   navIncrease("nav-num");
-
+  dynmicActivityValue("History", "fix-mobileBtn");
   document.getElementById("btn-shop").setAttribute("disabled", true);
 });
 
 document.getElementById("btn-cloudSync").addEventListener("click", function () {
+  let time = getCurrentTime();
+  alert(`Board updated successfully at ${time}`);
   reduceTask("task-no");
   navIncrease("nav-num");
+  dynmicActivityValue("History", "CloudSync-id");
   document.getElementById("btn-cloudSync").setAttribute("disabled", true);
 });
 
 document.getElementById("btn-swiftpay").addEventListener("click", function () {
+  let time = getCurrentTime();
+  alert(`Board updated successfully at ${time}`);
   reduceTask("task-no");
   navIncrease("nav-num");
+  dynmicActivityValue("History", "Home-page");
   document.getElementById("btn-swiftpay").setAttribute("disabled", true);
 });
 
 document.getElementById("btn-meta").addEventListener("click", function () {
+  let time = getCurrentTime();
+  alert(`Board updated successfully at ${time}`);
   reduceTask("task-no");
   navIncrease("nav-num");
+  dynmicActivityValue("History", "emoji");
   document.getElementById("btn-meta").setAttribute("disabled", true);
 });
 
 document.getElementById("btn-google").addEventListener("click", function () {
+  let time = getCurrentTime();
+  alert(`Board updated successfully at ${time}`);
   reduceTask("task-no");
   navIncrease("nav-num");
+  dynmicActivityValue("History", "OpenAI");
   document.getElementById("btn-google").setAttribute("disabled", true);
 });
 
 document.getElementById("btn-glassdoar").addEventListener("click", function () {
+  let time = getCurrentTime();
+  alert(`Board updated successfully at ${time}`);
+  alert(`congrats!!!you have completed the task `);
   reduceTask("task-no");
   navIncrease("nav-num");
+  dynmicActivityValue("History", "job-search");
   document.getElementById("btn-glassdoar").setAttribute("disabled", true);
+});
+
+document.getElementById("activity-btn").addEventListener("click", function () {
+  let data = document.getElementById("History");
+  data.innerText = "";
 });
--- utilities.js
+++ utilities.js
@@ -13,3 +13,52 @@ function navIncrease(navNum) {
   let newNum = improvednavNum + 1;
   document.getElementById(navNum).innerText = newNum;
 }
+// Function to add dynamic date
+function dateDynamic() {
+  const now = new Date();
+  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const day = days[now.getDay()];
+  const month = months[now.getMonth()];
+  const monthDate = now.getDate();
+  const year = now.getFullYear();
+  const pDate = document.getElementById("show-time");
+  pDate.innerText = `${day}, ${month} ${monthDate} ${year}`;
+}
+dateDynamic();
+
+//current time
+function getCurrentTime() {
+  const now = new Date();
+  let hours = now.getHours();
+  const minutes = now.getMinutes();
+  const ampm = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12 || 12;
+  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+  return `${hours}:${formattedMinutes} ${ampm}`;
+}
+// Activity Log
+
+function dynmicActivityValue(id, btnTitle) {
+  const btnT = document.getElementById(btnTitle);
+  const ShopEase = btnT.innerText;
+  const createElm = document.createElement("p");
+  let addvalue = document.getElementById(id);
+  const currentTime = getCurrentTime();
+  createElm.innerText = `user click on this ${ShopEase} at ${currentTime}`;
+  addvalue.appendChild(createElm);
+}
